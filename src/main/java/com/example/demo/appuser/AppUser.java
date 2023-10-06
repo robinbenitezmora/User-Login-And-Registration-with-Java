7ed854c518a5c03e9b6ce071a86d58a08f2078d2
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-05 21:57:31 -0500

    Create 'AppUser.java' file

---
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -0,0 +1,57 @@
+package com.example.demo.appuser;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AppUser implements UserDetails {
+
+ private Long id;
+ private String name;
+ private String username;
+ private String email;
+ private String password;
+ private AppUserRole appUserRole;
+ private Boolean locked;
+ private Boolean enabled;
+
+ @Override
+ public Collection<? extends GrantedAuthority> getAuthorities() {
+  SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+  return Collections.singletonList(authority);
+ }
+
+ @Override
+ public String getPassword() {
+  return password;
+ }
+
+ @Override
+ public String getUsername() {
+  return username;
+ }
+
+ @Override
+ public boolean isAccountNonExpired() {
+  return true;
+ }
+
+ @Override
+ public boolean isAccountNonLocked() {
+  return !locked;
+ }
+
+ @Override
+ public boolean isCredentialsNonExpired() {
+  return true;
+ }
+
+ @Override
+ public boolean isEnabled() {
+  return enabled;
+ }
+
+}
