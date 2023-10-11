0f99b8d4da9c93cc2d6a7f2f5ec56d7e501c8492
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-10 21:29:17 -0500

    Refactor 'AppUserService.java' file

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -7,8 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,68 +27,64 @@ import lombok.Setter;
 
 public class AppUser implements UserDetails {
 
- @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
- @Id
- @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
- private Long id;
- private String name;
- private String username;
- private String email;
- private String password;
- private AppUserRole appUserRole;
- private Boolean locked;
- private Boolean enabled;
+  @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+  private Long id;
+  private String name;
+  private String username;
+  private String email;
+  private String password;
+  @Enumerated(EnumType.STRING)
+  private AppUserRole appUserRole;
+  private Boolean locked;
+  private Boolean enabled;
 
- public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked,
-   Boolean enabled) {
-  this.name = name;
-  this.username = username;
-  this.email = email;
-  this.password = password;
-  this.appUserRole = appUserRole;
-  this.locked = false;
-  this.enabled = false;
- }
+  public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked,
+      Boolean enabled) {
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.appUserRole = appUserRole;
+    this.locked = false;
+    this.enabled = false;
+  }
 
- public enum AppUserRole {
-  USER,
-  ADMIN
- }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+    return Collections.singletonList(authority);
+  }
 
- @Override
- public Collection<? extends GrantedAuthority> getAuthorities() {
-  SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
-  return Collections.singletonList(authority);
- }
+  @Override
+  public String getPassword() {
+    return password;
+  }
 
- @Override
- public String getPassword() {
-  return password;
- }
+  @Override
+  public String getUsername() {
+    return username;
+  }
 
- @Override
- public String getUsername() {
-  return username;
- }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
- @Override
- public boolean isAccountNonExpired() {
-  return true;
- }
+  @Override
+  public boolean isAccountNonLocked() {
+    return !locked;
+  }
 
- @Override
- public boolean isAccountNonLocked() {
-  return !locked;
- }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
- @Override
- public boolean isCredentialsNonExpired() {
-  return true;
- }
-
- @Override
- public boolean isEnabled() {
-  return enabled;
- }
+  @Override
+  public boolean isEnabled() {
+    return enabled;
+  }
 
 }
