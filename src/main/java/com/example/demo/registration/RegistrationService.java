9bc8aafe8f597849c55da01fb847253891239983
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-16 21:19:11 -0500

    Try to correct file

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -2,8 +2,10 @@ package com.example.demo.registration;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.appuser.AppUser;
 import com.example.demo.appuser.AppUserRole;
 import com.example.demo.appuser.AppUserService;
+import com.example.demo.registration.token.ConfirmationToken;
 
 import lombok.AllArgsConstructor;
 
@@ -14,12 +16,17 @@ public class RegistrationService {
   private final AppUserService appUserService;
   private EmailValidator emailValidator;
 
-  public String register(RegistrationRequest request) {
+  public ConfirmationToken register(RegistrationRequest request) {
     boolean isValidEmail = emailValidator.test(request.getEmail());
     if (!isValidEmail) {
       throw new IllegalStateException("email not valid");
     }
     return appUserService.signUpUser(new AppUser(request.getFirstName(), request.getLastName(), request.getEmail(),
-        request.getPassword(), AppUserRole.USER));
+        request.getPassword(), AppUserRole.USER, isValidEmail, isValidEmail));
   }
+
+  public String confirmToken(String token) {
+    return appUserService.confirmToken(token);
+  }
+
 }
